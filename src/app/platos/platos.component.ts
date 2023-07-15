92caed8f0b5c68058f12a153c9275ab6bd90f16a
RammmonDEV <ramon.moncholi08@gmail.com>
2023-07-15 11:05:33 +0200

    anyadir entrante

--- src/app/platos/platos.component.ts
+++ src/app/platos/platos.component.ts
@@ -9,15 +9,19 @@ import { Platos } from '../class/platos';
 })
 export class PlatosComponent implements OnInit {
 
+  title_makis :  string  = "MAKIS";
+  title_temaki :  string  = "TEMAKIS";
+  title_nigiri :  string  = "NIGIRIS";
 
   arr_platos: Platos[] = [];
+  entrante_1: Platos = new Platos("edamame", "judias","",1.5, "../../assets/edamame.jpg", "entrante");
    platos: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
    platos_1_2: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
    platos2: Platos = new Platos("sushi_2", "Descripcion Estandar2", "alga", 1, "", "temaki");
    platos3: Platos = new Platos("sushi_3", "Descripcion Estandar2", "", 1, "", "nigiri");
   
   constructor(){
-    this.arr_platos.push(this.platos, this.platos_1_2, this.platos2);
+    this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos2,);
   }
 
   ngOnInit(): void {
