51faac36181a0aec6aab92e3012d3ec91865410f
RammmonDEV <ramon.moncholi08@gmail.com>
2023-07-15 10:39:35 +0200

    cambios card

--- src/app/platos/platos.component.ts
+++ src/app/platos/platos.component.ts
@@ -12,11 +12,12 @@ export class PlatosComponent implements OnInit {
 
   arr_platos: Platos[] = [];
    platos: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
+   platos_1_2: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
    platos2: Platos = new Platos("sushi_2", "Descripcion Estandar2", "alga", 1, "", "temaki");
    platos3: Platos = new Platos("sushi_3", "Descripcion Estandar2", "", 1, "", "nigiri");
   
   constructor(){
-    this.arr_platos.push(this.platos, this.platos2);
+    this.arr_platos.push(this.platos, this.platos_1_2, this.platos2);
   }
 
   ngOnInit(): void {
