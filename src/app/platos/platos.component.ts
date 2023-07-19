10f6d2e381078f4a1ed4a3fd51fc79bf65ae4249
Ramón Moncholí Ros <ramon.moncholi08@gmail.com>
2023-07-19 11:30:30 +0200

    sushis ngSwitch ngClas

--- src/app/platos/platos.component.ts
+++ src/app/platos/platos.component.ts
@@ -1,33 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Platos } from '../class/platos';
 
-
 @Component({
-  selector: 'app-platos',
-  templateUrl: './platos.component.html',
-  styleUrls: ['./platos.component.css']
+selector: 'app-platos',
+templateUrl: './platos.component.html',
+styleUrls: ['./platos.component.css']
 })
 export class PlatosComponent implements OnInit {
 
-  title_makis :  string  = "MAKIS";
-  title_temaki :  string  = "TEMAKIS";
-  title_nigiri :  string  = "NIGIRIS";
+title_makis: string = 'MAKIS';
+title_temaki: string = 'TEMAKIS';
+title_nigiri: string = 'NIGIRIS';
 
-  arr_platos: Platos[] = [];
-  entrante_1: Platos = new Platos("edamame", "judias","",1.5, "../../assets/edamame.jpg", "entrante");
-   platos: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
-   platos_1_2: Platos = new Platos("sushi_1", "Descripcion Estandar", "sesamo", 2, "../../assets/sushi_card1.jpg", "maki");
-   platos2: Platos = new Platos("sushi_2", "Descripcion Estandar2", "alga", 1, "", "temaki");
-   platos3: Platos = new Platos("sushi_3", "Descripcion Estandar2", "", 1, "", "nigiri");
-  
-  constructor(){
-    this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos2,);
-  }
+arr_platos: Platos[] = [];
+entrante_1: Platos = new Platos('edamame', 'judias', '', 1.5, '../../assets/edamame.jpg', 'entrante');
+platos: Platos = new Platos('sushi_1', 'Descripcion Estandar', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
+platos_1_2: Platos = new Platos('sushi_1', 'Descripcion Estandar', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
+platos2: Platos = new Platos('sushi_2', 'Descripcion Estandar2', 'alga', 1, '', 'temaki');
+platos3: Platos = new Platos('sushi_3', 'Descripcion Estandar2', '', 1, '', 'nigiri');
 
-  ngOnInit(): void {
-  }
+constructor() {
+this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos2);
+}
 
+ngOnInit(): void {
+}
 
 
 }
-
--- src/app/reserva/reserva.component.ts
+++ src/app/reserva/reserva.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reserva',
@@ -6,19 +6,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./reserva.component.css']
 })
 export class ReservaComponent implements OnInit {
-  @ViewChild('fechaInput') fechaInput!: ElementRef<HTMLInputElement>;
-
+ 
   ngOnInit(): void {
-    const fecha = this.fechaInput.nativeElement;
-
-    fecha.addEventListener("change", () => {
-      const dia = fecha.value.slice(0, 3);
-
-      if (dia === "Sun") {
-        fecha.disabled = true;
-      } else {
-        fecha.disabled = false;
-      }
-    });
+   
   }
 }
