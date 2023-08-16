1a57e63ee518a462298b089917eefe02da5b2325
Ramón Moncholí Ros <ramon.moncholi08@gmail.com>
2023-08-16 10:53:39 +0200

    footer y cambios platos

--- src/app/platos/platos.component.ts
+++ src/app/platos/platos.component.ts
@@ -17,11 +17,12 @@ entrante_1: Platos = new Platos('edamame', 'judias', '', 1.5, '../../assets/edam
 platos: Platos = new Platos('Maki Salmon', 'El maki de toda la vida', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
 platos_1_2: Platos = new Platos('Maki Perca', 'Un maki especial', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
 platos_1_3_cebollino = new Platos('Maki Cebollino', 'Un maki de verdura','',1, '../../assets/maki_cebollino.png','maki');
-platos2: Platos = new Platos('Temaki Salmon', 'Alga rellena de salmon', 'alga', 1, '../../assets/temaki_1.jpg', 'temaki');
-platos3: Platos = new Platos('Nigiri Salmon', 'Arroz con una lomo de salmón', '', 1, '../../assets/nigiri_1_salmon.jpg', 'nigiri');
+temaki1: Platos = new Platos('Temaki Salmon', 'Alga rellena de salmon', 'alga', 1, '../../assets/temaki_1.jpg', 'temaki');
+temaki2: Platos = new Platos('Temaki Atun', 'Clasico temaki de atun', 'alga', 2.5, '../../assets/temaki_2.jpg', 'temaki');
+nigiri1: Platos = new Platos('Nigiri Salmon', 'Arroz con una lomo de salmón', '', 1, '../../assets/nigiri_1_salmon.jpg', 'nigiri');
 
 constructor() {
-this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos_1_3_cebollino, this.platos2, this.platos3);
+this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos_1_3_cebollino, this.temaki1, this.temaki2, this.nigiri1);
 }
 
 ngOnInit(): void {
