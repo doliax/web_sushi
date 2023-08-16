5d137d114e4af1ef6238a202fbe38b579e71a130
Ramón Moncholí Ros <ramon.moncholi08@gmail.com>
2023-08-16 11:06:32 +0200

    mas platos

--- src/app/platos/platos.component.ts
+++ src/app/platos/platos.component.ts
@@ -13,16 +13,25 @@ title_temaki: string = 'TEMAKIS';
 title_nigiri: string = 'NIGIRIS';
 
 arr_platos: Platos[] = [];
+
+//Entrantes
 entrante_1: Platos = new Platos('edamame', 'judias', '', 1.5, '../../assets/edamame.jpg', 'entrante');
+entrante_2: Platos = new Platos('Ajigonomi', 'Pequeñas galletas saladas de arroz', 'soja', 0.5, '../../assets/ajigonomi.jpg', 'entrante');
+//Makis
 platos: Platos = new Platos('Maki Salmon', 'El maki de toda la vida', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
 platos_1_2: Platos = new Platos('Maki Perca', 'Un maki especial', 'sesamo', 2, '../../assets/sushi_card1.jpg', 'maki');
 platos_1_3_cebollino = new Platos('Maki Cebollino', 'Un maki de verdura','',1, '../../assets/maki_cebollino.png','maki');
-temaki1: Platos = new Platos('Temaki Salmon', 'Alga rellena de salmon', 'alga', 1, '../../assets/temaki_1.jpg', 'temaki');
-temaki2: Platos = new Platos('Temaki Atun', 'Clasico temaki de atun', 'alga', 2.5, '../../assets/temaki_2.jpg', 'temaki');
-nigiri1: Platos = new Platos('Nigiri Salmon', 'Arroz con una lomo de salmón', '', 1, '../../assets/nigiri_1_salmon.jpg', 'nigiri');
+
+//Temakis
+temaki1: Platos = new Platos('Temaki Salmón', 'Alga rellena de salmón', 'alga', 1, '../../assets/temaki_1.jpg', 'temaki');
+temaki2: Platos = new Platos('Temaki Atún', 'Clasico temaki de atún', 'alga', 2.5, '../../assets/temaki_2.jpg', 'temaki');
+
+//Nigiris
+nigiri1: Platos = new Platos('Nigiri Salmón', 'Arroz con una lomo de salmón', '', 1, '../../assets/nigiri_1_salmon.jpg', 'nigiri');
+nigiri2: Platos = new Platos('Nigir Atún', 'Arroz con una lomo de atún', '', 2, '../../assets/nigiri_2_salmon.jpg', 'nigiri')
 
 constructor() {
-this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos_1_3_cebollino, this.temaki1, this.temaki2, this.nigiri1);
+this.arr_platos.push(this.entrante_1, this.platos, this.platos_1_2, this.platos_1_3_cebollino, this.temaki1, this.temaki2, this.nigiri1, this.nigiri2, this.entrante_2);
 }
 
 ngOnInit(): void {
