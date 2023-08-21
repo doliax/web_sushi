94a96eaacf7a11343148cc488217c0ec28a0a396
Ramón Moncholí Ros <ramon.moncholi08@gmail.com>
2023-08-21 11:01:44 +0200

    form contacto v1

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+
+  contactForm: FormGroup
+
+
+  constructor(private FormBuilder: FormBuilder){
+    this.contactForm = this.FormBuilder.group({
+      nombre: ['', Validators.required],
+      email: ['', Validators.required],
+      mensaje: ['', Validators.required]
+    });
+  }
+
+
+  ngOnInit(): void {}
+
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      // Mostrar error
+      alert('Por favor, rellena todos los campos correctamente.');
+      return;
+    }else{
+  
+      alert('Mensaje enviado correctamente.');
+      //Limpiamos valores
+      this.contactForm.reset();
+    }
+  }
 
 }
